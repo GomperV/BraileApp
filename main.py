8593f555d82df36d04a4ccbf14c09f60e38e4aff
Jakub Konior <karfaj45@gmail.com>
2023-05-25 19:49:02 +0200

    model prawie ready

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import cv2
-import pytesseract
-
 import os
-import cv2
 import numpy as np
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.utils import to_categorical
 
 # Ścieżka do folderu zawierającego obrazy napisów Braille'a
 folder_path = "C:/Users/PC/PycharmProjects/BraileApp/BraileApp/img"
@@ -24,35 +22,49 @@ labels = []
 
 # Przetwarzanie obrazów napisów Braille'a
 def preprocess_image(image):
-    pass
+    # Przykładowa implementacja preprocessingu obrazu (zmniejszenie rozmiaru, normalizacja, itp.)
+    processed_image = cv2.resize(image, (28, 28))
+    return processed_image
 
+# Iterate over image files in the folder
+# Iterate over image files in the folder
+class_files = {}
+for image_name in os.listdir(folder_path):
+    image_path = os.path.join(folder_path, image_name)
+    image = cv2.imread(image_path)
+    processed_image = preprocess_image(image)
 
-for cls in classes:
-    cls_folder_path = os.path.join(folder_path, cls)
-    images = os.listdir(cls_folder_path)
-    for image_name in images:
-        image_path = os.path.join(cls_folder_path, image_name)
-        image = cv2.imread(image_path)
-        # Przetwórz obraz (zmniejszenie rozmiaru, normalizacja, itp.)
-        processed_image = preprocess_image(image)
+    # Extract the class from the first letter of the file name
+    image_class = image_name[0].lower()
+
+    if image_class in classes:
         data.append(processed_image)
-        labels.append(cls)
+        labels.append([image_class])  # Wrap the image_class in a list
 
+# Print file names from each class
+for class_name, files in class_files.items():
+    print(f"Class: {class_name}")
+    for file_name in files:
+        print(file_name)
+    print()
 # Konwertowanie danych do macierzy numpy
 data = np.array(data)
 labels = np.array(labels)
 
 # Wymiary obrazów napisów Braille'a
-img_height = 28
-img_width = 28
+img_height, img_width, img_channels = data.shape[1:]
 
+# One-hot encoding of labels
+label_mapping = {label: index for index, label in enumerate(classes)}
+encoded_labels = np.array([label_mapping[label[0]] for label in labels])
+encoded_labels = to_categorical(encoded_labels, num_classes=len(classes))
 
 # Podział danych na zbiór treningowy i walidacyjny
-train_data, val_data, train_labels, val_labels = train_test_split(data, labels, test_size=0.2)
+train_data, val_data, train_labels, val_labels = train_test_split(data, encoded_labels, test_size=0.2)
 
 # Budowanie modelu
 model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(img_height, img_width, 3)))
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(img_height, img_width, img_channels)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
@@ -66,4 +78,3 @@ model.fit(train_data, train_labels, validation_data=(val_data, val_labels), epoc
 
 # Zapisz wytrenowany model
 model.save("braille_model.h5")
-
---
+++ test.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+from keras.models import load_model
+
+from BraileApp.main import classes
+
+# Load the trained model
+model = load_model("braille_model.h5")
+save_path = r"C:\Users\PC\PycharmProjects\BraileApp\BraileApp\savepath\zdjecieTestowane"
+# Function to preprocess image
+def preprocess_image(image):
+    processed_image = cv2.resize(image, (28, 28))
+    save_path_with_extension = save_path + ".jpg"  # Specify the desired file extension
+    cv2.imwrite(save_path_with_extension, processed_image)  # Save the processed image
+    return processed_image
+
+# Function to predict the class of an image
+def predict_class(image):
+    preprocessed_image = preprocess_image(image)
+    input_image = np.expand_dims(preprocessed_image, axis=0)
+    predictions = model.predict(input_image)
+    predicted_class_index = np.argmax(predictions)
+    predicted_class = classes[predicted_class_index]
+    return predicted_class
+
+# Load and predict the class for a test image
+test_image_path = r"C:\Users\PC\PycharmProjects\BraileApp\BraileApp\example\testimg.jpg"
+test_image = cv2.imread(test_image_path)
+predicted_class = predict_class(test_image)
+print("Predicted class:", predicted_class)
